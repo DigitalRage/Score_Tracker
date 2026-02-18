c33b070db97586f2f70bdd3acf88b96898e6af45
richmondmason-bit <github email>
2026-02-18 09:34:33 -0700

    pseudocode

---
+++ Game_pseudocode.py
@@ -0,0 +1,25 @@
+#import modules(pygame,random and time)
+
+#pygame play game
+
+#player variables for size and speed
+
+#color codes
+
+#set screen width and lifetime#main game loop 
+#game over = false
+
+#x,y coordinate creation 
+#make circle food 
+
+#loop for player control
+#if player hits escape = pygame.quit
+#if statement to pygame.key for controls and movement 
+#if player is outside of screen area gameover = false 
+
+#fill screen with black background
+#update display
+
+#set clock speed to palyer speed 
+#if pygame = game over then print game over and time.sleep,game_loop 
+#game_loop 
